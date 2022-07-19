15fe67e1e66a4950ec9ccfe6ef939e273edde2ba
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-19 19:41:04 -0400

    naive-ui setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,29 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	css: [
-		'primevue/resources/themes/saga-blue/theme.css',
-		'primevue/resources/primevue.css',
-		'primeicons/primeicons.css',
-	],
+	build: {
+    transpile:
+      process.env.NODE_ENV === 'production'
+        ? [
+            'naive-ui',
+            'vueuc',
+            '@css-render/vue3-ssr',
+            '@juggle/resize-observer'
+          ]
+        : ['@juggle/resize-observer']
+  },
+	css: [],
 	vite: {
 		css: {
 				preprocessorOptions: {
-						scss: {
-								additionalData: '@import "@/assets/variables.scss";',
-						},
+						scss: { additionalData: '@import "@/assets/variables.scss";' },
 				},
 		},
+		optimizeDeps: {
+      include:
+        process.env.NODE_ENV === 'development'
+          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+          : []
+    },
 	},
 })
