effa2edab992e6f6d97a5abef182a8ab0387ea74
Dyoma3 <dinko.f.yoma@gmail.com>
2022-07-19 16:48:19 -0400

    uninstall primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	css: [
-		'primevue/resources/themes/saga-blue/theme.css',
-		'primevue/resources/primevue.css',
-		'primeicons/primeicons.css',
-	],
+	css: [],
 	vite: {
 		css: {
 				preprocessorOptions: {
-						scss: {
-								additionalData: '@import "@/assets/variables.scss";',
-						},
+						scss: { additionalData: '@import "@/assets/variables.scss";' },
 				},
 		},
 	},
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import InputText from 'primevue/inputtext';
 const message = ref<string>('');
 </script>
 <template>
 	<div class="display">
 		<div style="height: 20px">{{ message }}</div>
-		<input-text v-model="message" />
 	</div>
 </template>
 <style scoped>
